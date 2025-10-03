efd5b1a8599d5cf1936d58515cdab3ab0d68610d
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2025-10-03 10:58:59 +0300

    updated `@qavajs/validation` without chai dependency

--- test-e2e/cjs/step_definitions/custom.js
+++ test-e2e/cjs/step_definitions/custom.js
@@ -7,7 +7,7 @@ const {
     BeforeExecution,
     AfterExecution
 } = require('../../../index');
-const { expect } = require('chai');
+const { expect } = require('@qavajs/validation');
 
 let valueInCoordinator = 0;
 BeforeExecution(async function () {
@@ -15,7 +15,7 @@ BeforeExecution(async function () {
 });
 
 AfterExecution(async function () {
-    expect(valueInCoordinator).to.equal(1);
+    expect(valueInCoordinator).toEqual(1);
 });
 
 
@@ -39,43 +39,43 @@ When('I do smth async', async function() {
 });
 
 When('I verify that config loaded', async function() {
-    expect(this.config.defaultTimeout).to.equal(20000);
+    expect(this.config.defaultTimeout).toEqual(20000);
 });
 
 When('I verify that memory loaded', async function() {
-    expect(this.memory.getValue('$customValue')).to.equal('cjs');
+    expect(this.memory.getValue('$customValue')).toEqual('cjs');
 });
 
 When('I verify that memory is connected to qavajs world', async function() {
-    expect(this.getValue('$customValue')).to.equal('cjs');
-    expect(this.getValue('$additionalValue')).to.equal(12);
+    expect(this.getValue('$customValue')).toEqual('cjs');
+    expect(this.getValue('$additionalValue')).toEqual(12);
 });
 
 When('I verify that process env loaded', async function() {
-    expect(process.env.CONFIG).to.equal('test-e2e/cjs/config.js');
-    expect(process.env.PROFILE).not.to.be.undefined;
-    expect(process.env.MEMORY_VALUES).to.equal('{}');
-    expect(process.env.CLI_ARGV).to.include('--qavaBoolean --qavaValue 42');
-    expect(process.env.DEFAULT_TIMEOUT).to.equal('20000');
-    expect(process.env.CURRENT_SCENARIO_NAME).to.equal('verify process env');
+    expect(process.env.CONFIG).toEqual('test-e2e/cjs/config.js');
+    expect(process.env.PROFILE).not.toBeUndefined();
+    expect(process.env.MEMORY_VALUES).toEqual('{}');
+    expect(process.env.CLI_ARGV).toContain('--qavaBoolean --qavaValue 42');
+    expect(process.env.DEFAULT_TIMEOUT).toEqual('20000');
+    expect(process.env.CURRENT_SCENARIO_NAME).toEqual('verify process env');
 });
 
 When('I import cjs', async function() {
     const module = require('../../modules/module.cjs');
-    expect(module()).to.equal(`I'm cjs`);
+    expect(module()).toEqual(`I'm cjs`);
 });
 
 When('I import esm', async function() {
     const module = (await import('../../modules/module.mjs')).default;
-    expect(module()).to.equal(`I'm esm`);
+    expect(module()).toEqual(`I'm esm`);
 });
 
 When('I execute composite step', async function () {
     await this.executeStep('Nested step "42"');
     const customDataTable = new DataTable([['1', '2', '3']])
     await this.executeStep('Data table step:', customDataTable);
-    expect(this.memory.getValue('$nestedValue')).to.equal('42');
-    expect(this.memory.getValue('$dataTable')).to.deep.equal({ rawTable: [['1', '2', '3']]});
+    expect(this.memory.getValue('$nestedValue')).toEqual('42');
+    expect(this.memory.getValue('$dataTable')).toDeepEqual({ rawTable: [['1', '2', '3']]});
 });
 
 When('Nested step {string}', async function(val) {
@@ -87,12 +87,12 @@ When('Data table step:', function (dataTable) {
 });
 
 When('Read memory {value} from cucumber type', async function(memoryValue) {
-    expect(memoryValue.value()).to.equal('cjs');
+    expect(memoryValue.value()).toEqual('cjs');
 });
 
 When('write {string} to {value} value', async function(value, key) {
     key.set(value);
-    expect(this.memory.getValue('$'+key.expression)).to.equal(value);
+    expect(this.memory.getValue('$'+key.expression)).toEqual(value);
 });
 
 When('I expect {value} {validation} {value}', async function(value1, validate, value2) {
--- test-e2e/esm/step_definitions/custom.js
+++ test-e2e/esm/step_definitions/custom.js
@@ -1,5 +1,5 @@
 import { DataTable, When, Override, Fixture, Template, AfterExecution, BeforeExecution } from '../../../index.mjs';
-import { expect } from 'chai';
+import { expect } from '@qavajs/validation';
 import moduleESM from '../../modules/module.mjs';
 import moduleCJS from '../../modules/module.cjs';
 
@@ -9,7 +9,7 @@ BeforeExecution(async function () {
 });
 
 AfterExecution(async function () {
-    expect(valueInCoordinator).to.equal(1);
+    expect(valueInCoordinator).toEqual(1);
 });
 
 Fixture('testFixture', async function() {
@@ -33,36 +33,36 @@ When('I do smth async', async function() {
 });
 
 When('I verify that config loaded', async function() {
-    expect(this.config.defaultTimeout).to.equal(20000);
+    expect(this.config.defaultTimeout).toEqual(20000);
 });
 
 When('I verify that memory loaded', async function() {
-    expect(this.memory.getValue('$customValue')).to.equal('esm');
+    expect(this.memory.getValue('$customValue')).toEqual('esm');
 });
 
 When('I verify that process env loaded', async function() {
-    expect(process.env.CONFIG).to.equal('test-e2e/esm/config.js');
-    expect(process.env.PROFILE).not.to.be.undefined;
-    expect(process.env.MEMORY_VALUES).to.equal('{}');
-    expect(process.env.CLI_ARGV).to.include('--qavaBoolean --qavaValue 42');
-    expect(process.env.DEFAULT_TIMEOUT).to.equal('20000');
-    expect(process.env.CURRENT_SCENARIO_NAME).to.equal('verify process env');
+    expect(process.env.CONFIG).toEqual('test-e2e/esm/config.js');
+    expect(process.env.PROFILE).not.toBeUndefined();
+    expect(process.env.MEMORY_VALUES).toEqual('{}');
+    expect(process.env.CLI_ARGV).toContain('--qavaBoolean --qavaValue 42');
+    expect(process.env.DEFAULT_TIMEOUT).toEqual('20000');
+    expect(process.env.CURRENT_SCENARIO_NAME).toEqual('verify process env');
 });
 
 When('I import cjs', async function() {
-    expect(moduleCJS()).to.equal(`I'm cjs`)
+    expect(moduleCJS()).toEqual(`I'm cjs`)
 });
 
 When('I import esm', async function() {
-    expect(moduleESM()).to.equal(`I'm esm`)
+    expect(moduleESM()).toEqual(`I'm esm`)
 });
 
 When('I execute composite step', async function () {
     await this.executeStep('Nested step "42"');
     const customDataTable = new DataTable([['1', '2', '3']])
     await this.executeStep('Data table step:', customDataTable);
-    expect(this.memory.getValue('$nestedValue')).to.equal('42');
-    expect(this.memory.getValue('$dataTable')).to.deep.equal({ rawTable: [['1', '2', '3']]});
+    expect(this.memory.getValue('$nestedValue')).toEqual('42');
+    expect(this.memory.getValue('$dataTable')).toDeepEqual({ rawTable: [['1', '2', '3']]});
 });
 
 When('Nested step {string}', async function(val) {
@@ -74,12 +74,12 @@ When('Data table step:', function (dataTable) {
 });
 
 When('Read memory {value} from cucumber type', async function(memoryValue) {
-    expect(memoryValue.value()).to.equal('esm');
+    expect(memoryValue.value()).toEqual('esm');
 });
 
 When('write {string} to {value} value', async function(value, key) {
     key.set(value);
-    expect(this.memory.getValue('$'+key.expression)).to.equal(value);
+    expect(await this.memory.getValue('$'+key.expression)).toEqual(value);
 });
 
 When('I expect {value} {validation} {value}', async function(value1, validate, value2) {
