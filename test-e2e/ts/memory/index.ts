6032dabb062f5e967c42d2be80697878599e823c
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2025-10-11 15:51:57 +0300

    removed hardcoded `swc` flag in favor of project specific configuration

--- test-e2e/ts/memory/index.ts
+++ test-e2e/ts/memory/index.ts
@@ -1,5 +1,10 @@
 export class Constants {
     customValue = 'ts';
+    getterSetter = {
+        _value: 42,
+        get value() { return this._value; },
+        set value(v1) { this._value = v1; },
+    }
 }
 
 export default {
